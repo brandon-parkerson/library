e2afd6be9b59b8b3b2268f1a576a11f3723ae170
brandon-parkerson <166299546+brandon-parkerson@users.noreply.github.com>
2024-05-13 00:03:37 +0300

    Display each book in list

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
-const show = document.querySelector("div");
+const card = document.querySelector(".card");
+card.innerHTML = "";
+addToLibrary();
 
 function Book(title, author, pages, readStatus) {
     this.author = author;
@@ -8,13 +10,15 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus;
 
     this.info = function() {
-        let readStatusStr = this.readStatus ? "read" : "not read yet";
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatusStr}`;
+        //let readStatusStr = this.readStatus ? "read" : "no";
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
     };
 };
-// let theHobbit = new Book("JRR Tolkein", "The Hobbit", 295, true);
+let theHobbit = new Book("JRR Tolkein", "The Hobbit", 295, "read");
+let myBook = new Book("Brandon Parkerson", "My Life", 200, "read");
 // console.log(theHobbit.info());
-
+myLibrary.push(theHobbit);
+myLibrary.push(myBook);
 
 
 function addToLibrary() {
@@ -22,13 +26,24 @@ function addToLibrary() {
     let title = prompt("Title: ");
     let author = prompt("Author: ");
     let pages = prompt("Pages: ");
-    let readStatus = prompt("Read status (true or false): ");
+    let readStatus = prompt("Read status (read or not read yet): ");
     // Push inputs to array
     //myLibrary.push(title, author, pages, readStatus);
     let newBook = new Book(title, author, pages, readStatus);
     myLibrary.push(newBook);
 };
 
-addToLibrary();
 
-show.innerHTML += myLibrary;
+
+
+
+
+for (let i = 0; i < myLibrary.length; i++) {
+    let paragraph = document.createElement("p");
+    console.log(myLibrary[i]);
+    let currentBook = myLibrary[i];
+    paragraph.innerText = currentBook.info();
+    card.appendChild(paragraph);
+};
+
+
