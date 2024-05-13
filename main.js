d654f48a8d24e0448b73bc846379bff587c8e3c6
brandon-parkerson <166299546+brandon-parkerson@users.noreply.github.com>
2024-05-13 23:32:20 +0300

    get input values

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+const newBookBtn = document.querySelector(".new-book-btn");
+const form = document.querySelector("#form");
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const bookPages = document.querySelector("#pages");
+const bookReadStatus = document.querySelector("#read-status");
+const submit = document.querySelector("#submit");
+
 const myLibrary = [];
+
+
+
+newBookBtn.addEventListener("click", addBookToLibrary);
+
+
 
 function Book(title, author, pages, readStatus) {
   // the constructor...
@@ -6,9 +20,26 @@ function Book(title, author, pages, readStatus) {
   this.author = author;
   this.pages = pages;
   this.readStatus = readStatus;
-}
+};
 
 function addBookToLibrary() {
   // do stuff here
-  
-}
+    form.style.display = "flex";
+    let title = bookTitle.value;
+    let author = bookAuthor.value;
+    let pages = bookPages.value;
+    let readStatus = bookReadStatus.value;
+
+    let newBook = new Book(title, author, pages, readStatus);
+    
+    submit.addEventListener("click", function(event){
+        event.preventDefault();
+        myLibrary.push(newBook);
+        form.reset();
+        console.log(myLibrary);
+    });
+    
+    //myLibrary.push(newBook);
+
+
+};
