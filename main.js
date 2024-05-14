0f34a4084250362018ce5ceec63103bc0427dfd3
brandon-parkerson <166299546+brandon-parkerson@users.noreply.github.com>
2024-05-14 11:38:31 +0300

    Add toggle and remove buttons.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const bookAuthor = document.querySelector("#author");
 const bookPages = document.querySelector("#pages");
 const bookReadStatus = document.querySelector("#read-status");
 const submit = document.querySelector("#submit");
+const library = document.querySelector(".library");
 
 const myLibrary = [];
 
@@ -22,6 +23,15 @@ function Book(title, author, pages, readStatus) {
   this.author = author;
   this.pages = pages;
   this.readStatus = readStatus;
+  if (this.readStatus === true) {
+    this.readStatus = `already read`;
+  }
+  else if (this.readStatus === false) {
+    this.readStatus = `have not read`;
+  };
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
+  }
 };
 
 submit.addEventListener("click", (event) => {
@@ -37,4 +47,38 @@ submit.addEventListener("click", (event) => {
   form.reset();
   form.style.display = "none";
   console.log(myLibrary);
+  createLibrary();
 });
+
+function createLibrary() {
+  library.innerHTML = ``;
+  myLibrary.forEach((book, index) => {
+    let card = document.createElement("div");
+    card.innerText = book.info();
+    library.appendChild(card);
+
+    let toggleButton = document.createElement("button");
+    toggleButton.classList.add("toggle")
+    toggleButton.textContent = book.readStatus === "already read" ? "Mark as Unread" : "Mark as Read";
+    toggleButton.addEventListener("click", () => toggleReadStatus(index));
+    card.appendChild(toggleButton);
+
+    let remove = document.createElement("button");
+    remove.textContent = "Remove";
+    remove.classList.add("remove");
+    remove.setAttribute("data-index-number", index);
+    remove.addEventListener("click", removeBook);
+    card.appendChild(remove);
+  })
+}
+
+function removeBook(event) {
+  let index = event.target.getAttribute("data-index-number");
+  myLibrary.splice(index, 1);
+  createLibrary();
+};
+
+function toggleReadStatus(index) {
+  myLibrary[index].readStatus = myLibrary[index].readStatus === "already read" ? "have not read" : "already read";
+  createLibrary();
+}
